e25e8488a110c4c973571c4ea89cf1fb44f92dd7
Alvaro Farina <farinalvaro@gmail.com>
2019-12-06 22:26:23 -0300

    changing all styles to styled components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
+import {
+    clearItemFromCart,
+    addItem,
+    removeItem
+} from '../../redux/cart/cart.actions';
 
-import './checkout-item.styles.scss';
+import {
+    CheckoutItemContainer,
+    ImageContainer,
+    QuantityContainer,
+    RemoveButtonContainer,
+    TextContainer
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
     return (
-        <div className="checkout-item">
-            <div className="image-container">
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt="item" />
-            </div>
-            <span className="name">{name}</span>
-            <span className="quantity">
-                <div onClick={() => removeItem(cartItem)} className="arrow">
+            </ImageContainer>
+            <TextContainer>{name}</TextContainer>
+            <QuantityContainer>
+                <div onClick={() => removeItem(cartItem)}>
                     &#10094;
                 </div>
-                <span className="value">
+                <span>
                     {quantity}
                 </span>
-                <div onClick={() => addItem(cartItem)} className="arrow">
+                <div onClick={() => addItem(cartItem)}>
                     &#10095;
                 </div>
-            </span>
-            <span className="price">{price}</span>
-            <div onClick={() => clearItem(cartItem)} className="remove-button">
+            </QuantityContainer>
+            <TextContainer>{price}</TextContainer>
+            <RemoveButtonContainer onClick={() => clearItem(cartItem)}>
                 &#10005;
-            </div>
-        </div>
-    )
+            </RemoveButtonContainer>
+        </CheckoutItemContainer>
+    );
 };
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
-    <div className="homepage">
-        <div className="directory-menu">
-            <Directory />
-        </div>
-    </div>
-)
+    <HomePageContainer>
+        <Directory />
+    </HomePageContainer>
+);
 
 export default HomePage;
