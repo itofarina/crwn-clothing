495bdfb1c4976311bba5d1cfd5e2a3c3b61a7e05
Alvaro Farina <farinalvaro@gmail.com>
2019-12-18 17:14:42 -0300

    pulling shop data from firebase and storing it in the shop reducer - also included withSpinner HOC

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+
 
 class App extends React.Component {
 
@@ -70,7 +72,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled.div`
@@ -23,7 +23,15 @@ export const OptionsContainer = styled.div`
     justify-content: flex-end;
 `;
 
-export const OptionLink = styled(Link)`
+const optionStyles = css`
     padding: 10px 15px;
     cursor: pointer;
+`;
+
+export const OptionLink = styled(Link)`
+    ${optionStyles}
+`;
+
+export const OptionDiv = styled.div`
+    ${optionStyles}
 `;
